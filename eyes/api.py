53bc27bdcdd14be436e9a13c46ae40997cc3a052
Noah Altunian <naltunian@deltarisk.com>
2019-06-06 15:38:25 -0400

    Add shodan key constant, fix lint errors

--- eyes/api.py
+++ eyes/api.py
@@ -1,7 +1,13 @@
-import requests
+"""This file contains the core API for eyes.py."""
 import socket
+import requests
+
+SHODAN_KEY = None
 
 class API:
+    """This class contains the core API for for the eyes.py package.
+    Every method within API() is a classmethod, eg. `API.<classmethod>()'
+    """
 
     @classmethod
     def whois(self, target):
@@ -15,12 +21,15 @@ class API:
         """Takes the user specified domain or IP address and performs a Cloudflare detection scan."""
         dns = 'http://api.hackertarget.com/dnslookup/?q=' + target
         req = requests.get(dns)
+        res = None
         if 'cloudflare' in req.text:
-            return "\n**Cloudflare detected**"
+            res = "\n**Cloudflare detected**"
         else:
-            return f"{target} is *not* protected by Cloudflare"
+            res = f"{target} is *not* protected by Cloudflare"
 
-      
+        return res
+
+
     @classmethod
     def zonetransfer(self, target):
         """Takes the user specified domain or IP address and performs a zone transfer scan."""
@@ -47,32 +56,35 @@ class API:
         """Takes the user specified IP address and performs a honeypot probability scan.
         Note, this requires a valid shodan.io key.
         """
-        honey = f"https://api.shodan.io/labs/honeyscore/{target}?key="
+        if not SHODAN_KEY:
+            return "This feature requires a Shodan API key to be set in `eyes/api.py:4. Please do that."
+        honey = f"https://api.shodan.io/labs/honeyscore/{target}?key={SHODAN_KEY}"
         phoney = requests.get(honey).text
+        prob = None
         if '0.0' in phoney:
-            return 'Honeypot Probability: 0%'
+            prob = 'Honeypot Probability: 0%'
         elif '0.1' in phoney:
-            return 'Honeypot Probability: 10%'
+            prob = 'Honeypot Probability: 10%'
         elif '0.2' in phoney:
-            return 'Honeypot Probability: 20%'
+            prob = 'Honeypot Probability: 20%'
         elif '0.3' in phoney:
-            return 'Honeypot Probability: 30%'
+            prob = 'Honeypot Probability: 30%'
         elif '0.4' in phoney:
-            return 'Honeypot Probability: 40%'
+            prob = 'Honeypot Probability: 40%'
         elif '0.5' in phoney:
-            return 'Honeypot Probability: 50%'
+            prob = 'Honeypot Probability: 50%'
         elif '0.6' in phoney:
-            return 'Honeypot Probability: 60%'
+            prob = 'Honeypot Probability: 60%'
         elif '0.7' in phoney:
-            return 'Honeypot Probability: 70%'
+            prob = 'Honeypot Probability: 70%'
         elif '0.8' in phoney:
-            return 'Honeypot Probability: 80%'
+            prob = 'Honeypot Probability: 80%'
         elif '0.9' in phoney:
-            return 'Honeypot Probability: 90%'
+            prob = 'Honeypot Probability: 90%'
         elif '1.0' in phoney:
-            return 'Honeypot Probability: 100%'
-        else:
-            return 'Something may have gone wrong checking honeypot probability.'
+            prob = 'Honeypot Probability: 100%'
+
+        return prob
 
 
     @classmethod
@@ -81,7 +93,7 @@ class API:
         robot = f"http://{target}/robots.txt"
         return requests.get(robot).text
 
-    
+
     @classmethod
     def getlinks(self, target):
         """Takes the user specified domain or IP address and retrieves all links."""
@@ -94,23 +106,24 @@ class API:
         geo = f"http://ipinfo.io/{target}/geo"
         return requests.get(geo).text
 
-    
+
     @classmethod
     def traceroute(self, target):
         """Takes the user specified domain or IP address and performs a traceroute review."""
         trace = 'https://api.hackertarget.com/mtr/?q=' + target
         return requests.get(trace).text
 
-    
+
     @classmethod
     def getip(target):
         """Takes the user specified domain name and returns its IP address."""
-        ipAddr = socket.gethostbyname(target)
-        return f"{target}'s IP address is {ipAddr}."
+        ip_addr = socket.gethostbyname(target)
+        return f"{target}'s IP address is {ip_addr}."
 
-    
+
     @classmethod
     def about(self):
+        """Prints an 'about the program' message."""
         return "This program was created by Noah Altunian, and was adapted " \
                 "from github.com/naltun/eyes.sh, which was adapted from " \
                 "github.com/s0md3v/ReconDog. It is licensed under the GNU GPLv2."

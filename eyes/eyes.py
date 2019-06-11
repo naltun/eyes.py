8848dd3044f6ec165ab2f4b1456b5c7fc54fb394
Noah Altunian <naltunian@deltarisk.com>
2019-06-11 08:57:12 -0400

    Fix lint errors in eyes.py

--- eyes/eyes.py
+++ eyes/eyes.py
@@ -1,18 +1,21 @@
-import requests
-import socket
-
-from api import API
+"""This file contains the API for the Eyes class, which
+is used for creating the CLI environment for eyes.py.
+"""
 from colorama import init, Fore, Style
+from .api import API
 
 # filter ANSI escape sequences from any STDOUT/STDERR and replace with Win32 calls
 # this will have no effect if running this package on other platforms
 init()
 
 class Eyes:
-    
+    """This class holds the CLI API. All methods for prompting for
+    and processing user input exists here.
+    """
+
     # banner created with figlet.js (github.com/patorjk/figlet.js)
     @classmethod
-    def banner(self):
+    def banner(cls):
         """Prints the banner logo for eyes.py"""
         return Fore.LIGHTGREEN_EX + "______\n" \
             "|  ___|\n" \
@@ -22,10 +25,10 @@ class Eyes:
             "\\____/\\__, |\\___||___/ " + Fore.WHITE + "v" + Fore.LIGHTRED_EX + "0.1beta" + Fore.LIGHTGREEN_EX + "\n" \
             "       __/ |\n" \
             "       |___/\n"
- 
-    
+
+
     @classmethod
-    def menu(self):
+    def menu(cls):
         """Prints the menu for interacting with eyes.py"""
         return Fore.WHITE + "1. Whois Lookup\n" \
             "2. DNS Lookup + Cloudflare Detector\n" \
@@ -43,7 +46,7 @@ class Eyes:
 
 
     @classmethod
-    def eyes(self):
+    def eyes(cls):
         """The features of eyes.py get called here"""
         choice = input('What do you want to do? ')
 
@@ -61,12 +64,12 @@ class Eyes:
             domain = input('Enter a domain: ')
             print(API.zonetransfer(domain))
             Eyes.display()
-        
+
         elif choice == '4':
             target = input('Enter a domain or IP address: ')
             print(API.portscan(target))
             Eyes.display()
-        
+
         elif choice == '5':
             target = input('Enter a domain or IP address: ')
             print(API.getheader(target))
@@ -76,7 +79,7 @@ class Eyes:
             target = input('Enter IP address: ')
             print(API.honeypot(target))
             Eyes.display()
-        
+
         elif choice == '7':
             answer = input('This feature makes a direct call to the target -- would you like to continue? [Y/n] ')
             if answer == 'y':
@@ -99,7 +102,7 @@ class Eyes:
             target = input('Enter IP address: ')
             print(API.iplocate(target))
             Eyes.display()
-        
+
         elif choice == '10':
             target = input('Enter a domain or IP address: ')
             print(API.traceroute(target))
@@ -109,22 +112,23 @@ class Eyes:
             target = input('Enter a domain: ')
 
             Eyes.display()
-        
+
         elif choice == '12':
             print(API.about())
             Eyes.display()
-        
+
         elif choice == '13':
             print('Bye')
             print(Style.RESET_ALL)
             exit(0)
-        
+
         else:
             print('Your choice is invalid.')
             Eyes.display()
 
 
     @classmethod
-    def display(self):
+    def display(cls):
+        """Outputs the Eyes menu as well as prompt the user for menu item selection."""
         print("\n" + Eyes.menu() + "\n")
         print("\n" + Eyes.eyes())
